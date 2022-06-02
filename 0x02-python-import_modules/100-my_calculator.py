08c791d73ef636979e04201eafbae22f4e51fb89
Getasew Tarekegn <getasew6374@gmail.com>
2022-06-02 05:30:14 -0700

    Rask 10

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+
+        """
+
+            Handles basic operations
+
+                Performs basic operations like addition, substraction,
+
+                    multiplication and division between two numbers.
+
+                        The program will execute an operation between two numbers
+
+                            selected by the operator sent to the program.
+
+                                """
+
+                                    l_av = len(argv) - 1
+
