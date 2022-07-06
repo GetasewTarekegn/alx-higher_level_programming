77e63b05aac0933af8401eb7c110601f950ba366
GetasewTarekegn <34296833+GetasewTarekegn@users.noreply.github.com>
2022-07-06 08:03:18 +0300

    commit

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-from json import loads
+"""returns python data structure"""
+import json
 
 
 def from_json_string(my_str):
-    return loads(my_str)
+    """returns an object(Python data structure)
+    represented by a json string"""
+    data = json.loads(my_str)
+    return data
