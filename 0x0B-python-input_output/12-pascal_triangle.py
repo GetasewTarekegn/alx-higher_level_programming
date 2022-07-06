476403d79baf034f93254ab385c7da6eb32d2bd4
GetasewTarekegn <34296833+GetasewTarekegn@users.noreply.github.com>
2022-07-06 08:01:36 +0300

    commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
+"""Pascal's triangle
+"""
+
+
 def pascal_triangle(n):
+    """returns a list of intergers representing the
+    pascal's triangle of n"""
     if n <= 0:
         return []
-
-    limit = n - 1
-    triangle = [[1]]
-
-    for i in range(limit):
-        row = []
-        row.append(1)
-
-        if len(triangle[i]) > 1:
-            prev_row_len = len(triangle[i]) - 1
-            nxt = 1
-
-            for j in range(prev_row_len):
-                suma = triangle[i][j] + triangle[i][nxt]
-                row.append(suma)
-                nxt += 1
-
-        row.append(1)
-        triangle.append(row)
-
+    a = [[] for i in range(n)]
+    for i in range(n):
+        for j in range(i + 1):
+            if j < i:
+                if j == 0:
+                    a[i].append(1)
+                else:
+                    a[i].append(a[i - 1][j] + a[i - 1][j - 1])
+            elif j == i:
+                a[i].append(1)
     return 
