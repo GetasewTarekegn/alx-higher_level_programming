0e5fe8c9ec10d11c0f770f4ed0bbe4e709cee958
GetasewTarekegn <34296833+GetasewTarekegn@users.noreply.github.com>
2022-07-06 08:00:51 +0300

    commit

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
+"""Student class defines a student"""
+
+
 class Student:
+    """defines a student"""
     def __init__(self, first_name, last_name, age):
+        """instantiation"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        class_d = self.__dict__
-        sel_d = dict()
-
-        if type(attrs) is list:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return class_d
-
-                if attr in class_d:
-                    sel_d[attr] = class_d[attr]
-
-            return sel_d
-
-        return class_d
+        """retrieves a dictionary representation of
+        Student instance"""
+        if type(attrs) == list and all(type(elem) == str for elem in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        for i in json:
-            if i in self.__dict__.keys():
-                self.__dict__[i] = json[i]
+        """replaces all attributes of the student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
