e13967282d126631b62bb76507d29abf629ca106
GetasewTarekegn <34296833+GetasewTarekegn@users.noreply.github.com>
2022-07-06 08:02:50 +0300

    commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from json import dumps
+"""returns json representation of an object(string)"""
+import json
 
 
 def to_json_string(my_obj):
-    return dumps(my_obj)
+    """returns the json representation of an object"""
+    data = json.dumps(my_obj)
+    return data
