538603ba4322ac779ff6f670da8702ddabe25272
GetasewTarekegn <34296833+GetasewTarekegn@users.noreply.github.com>
2022-08-10 11:32:09 +0300

    commit

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Insert to a table
-INSERT INTO `first_table` (`id`, `name`) VALUES ('89', "Holberton School");
+-- inserts a new row in the table first_table
+-- database name is passes as argument of the mysql command
+INSERT INTO first_table (`id`, `name`) VALUES (89, "Holberton School");
